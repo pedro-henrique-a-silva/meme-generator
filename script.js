3a3c534745c2751e268fb1b6cd2c3b547c171c6a
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-26 11:30:17 -0300

    Requisito 2 concluido, adicionando imagem

--- script.js
+++ script.js
@@ -2,10 +2,23 @@ const inputTexto = document.querySelector('#text-input');
 const memeImageContainer = document.querySelector('#meme-image-container');
 const memeText = document.querySelector('#meme-text');
 
+const memeImage = document.querySelector('#meme-image');
+
 const escreveTextoNoMeme = () => {
   inputTexto.addEventListener('keyup', () => {
     memeText.innerHTML = inputTexto.value;
   });
 };
 
+const uploadImageToPreview = () => {
+  const memeInsert = document.querySelector('#meme-insert');
+  memeInsert.addEventListener('change', (event) => {
+    const image = event.target.files[0];
+    console.log(event.target);
+    console.log(URL.createObjectURL(image));
+    memeImage.src = URL.createObjectURL(image);
+  });
+};
+
 escreveTextoNoMeme();
+uploadImageToPreview();
